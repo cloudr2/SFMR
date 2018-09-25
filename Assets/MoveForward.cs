ece699e3c8647c8774b7f2658c8dd716ff759b9e
cloudr2 <alejandro.popovsky@gmail.com>
2018-09-25 01:50:39 -0300

    Initial commit

---
+++ Assets/MoveForward.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour {
+
+    public float speed;
+
+	void Update () {
+
+        if (Input.GetKey(KeyCode.R)) {
+            transform.position = Vector3.zero;
+        }
+
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+	}
+}
